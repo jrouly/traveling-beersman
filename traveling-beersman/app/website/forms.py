8d7a2b218e7ea477f292c402a07c215f06d36477
Jean Michel Rouly <michel.rouly@gmail.com>
2015-02-21 15:15:47 -0500

    Added a validator to the form

--- traveling-beersman/app/website/forms.py
+++ traveling-beersman/app/website/forms.py
@@ -4,3 +4,8 @@ from wtforms import TextField
 class CityForm(Form):
     city_name = TextField('City')
     address = TextField('Address')
+
+    def validate_city_name(form, field):
+        if len(form.city_name.data) == 0 and len(form.address.data) == 0:
+            raise ValidationError("You must enter at least the address or city.")
+
