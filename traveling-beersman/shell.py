35a7297dfae18b21b69a34541a1b4dd2798b258d
Jean Michel Rouly <michel.rouly@gmail.com>
2015-02-21 12:06:01 -0500

    Fixed borked indentation

--- traveling-beersman/shell.py
+++ traveling-beersman/shell.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-    import os
-    import readline
-    from pprint import pprint
+import os
+import readline
+from pprint import pprint
 
-    from flask import *
-    from app import *
+from flask import *
+from app import *
 
-    os.environ['PYTHONINSPECT'] = 'True'
+os.environ['PYTHONINSPECT'] = 'True'
