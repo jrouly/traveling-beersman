fdae8c05a3c186b66c23469ddf3144104d43d011
Jean Michel Rouly <michel.rouly@gmail.com>
2015-02-21 14:38:23 -0500

    Pseudo coded the views

--- traveling-beersman/app/website/views.py
+++ traveling-beersman/app/website/views.py
@@ -4,6 +4,22 @@ mod = Blueprint('website', __name__, url_prefix='/')
 
 @mod.route('/')
 def home():
-    return render_template("website/home.html")
 
+    # if the user enters a city name:
+    if False:
+        pass
 
+        # if city name exists in database:
+            # geojson = get(city name)
+
+        # else:
+            # soup = scrape-BA
+            # locations = raw_locations(soup)
+            # locations = geocoded_locations(locations)
+            # locations = TSP(locations)
+            # geojson = format(locations)
+
+        # render_template("website/map.html", city=city name, data=geojson)
+
+    else:
+        return render_template("website/home.html")
